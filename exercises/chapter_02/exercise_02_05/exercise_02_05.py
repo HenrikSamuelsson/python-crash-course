f81628c4dc6de6ae07827b8dd30ab6487d40f551
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2017-04-22 13:15:43 +0200

    Add solution for exercis 2.6.

--- exercises/chapter_02/exercise_02_05/exercise_02_05.py
+++ exercises/chapter_02/exercise_02_05/exercise_02_05.py
@@ -1,3 +1,2 @@
 # 2-5 Famous Quote
-print('"Success is a lousy teacher. It seduces smart \npeople into thinking they can not lose." \n\n\tBill Gates'
-)
+print('"Success is a lousy teacher. It seduces smart \npeople into thinking they can not lose." \n\n\tBill Gates')
---
+++ exercises/chapter_02/exercise_02_06/exercise_02_06.py
@@ -0,0 +1,4 @@
+# 2-6 Famous Qoute 2
+famous_person = "Bill Gates"
+message = '"Success is a lousy teacher. It seduces smart \npeople into thinking they can not lose." \n\n\t' + famous_person
+print(message)

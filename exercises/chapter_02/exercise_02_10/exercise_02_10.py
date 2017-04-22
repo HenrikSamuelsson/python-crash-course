2f508798b0a5f78ef6c04302e6983e4b824a2df0
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2017-04-22 17:27:29 +0200

    Add solution to exercise 2-10.

---
+++ exercises/chapter_02/exercise_02_10/exercise_02_10.py
@@ -0,0 +1,15 @@
+# 2-10 Adding Comments
+
+# Below follows the code from exercise 2-3 and 2-4 again but now with (more) comments
+
+# 2-3 Personal Message
+name = "Henrik"  # variable for storing user name
+
+# concatenate the user name with some strings to form a personal message
+print("Hello " + name + ", lets learn some Python today!") #
+ 
+# 2-4 Name Cases
+name = "henrik samuelsson"
+print(name.lower())  # change all letters to lower case and print
+print(name.upper())  # change all letters to upper case and print
+print(name.title())	 # change the first letter of the names to upper case and print

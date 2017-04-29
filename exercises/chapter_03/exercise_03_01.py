3237345bdc725fc631b208ea705cb41b722bcd1f
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2017-04-29 09:48:32 +0200

    Add solution to exercise 3.1.

---
+++ exercises/chapter_03/exercise_03_01.py
@@ -0,0 +1,8 @@
+# 3-1 Names
+
+friend_names = ["Albert Einstein", "Isac Newton", "Marie Curie", "Galileo Galilei"]
+
+print(friend_names[0])
+print(friend_names[1])
+print(friend_names[2])
+print(friend_names[3])

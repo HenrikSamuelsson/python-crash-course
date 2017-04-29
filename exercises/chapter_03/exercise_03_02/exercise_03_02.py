d8c0285b6b15c28dcd368849321600863278b9d5
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2017-04-29 09:53:50 +0200

    Add solution to exercise 3.2.

---
+++ exercises/chapter_03/exercise_03_02/exercise_03_02.py
@@ -0,0 +1,15 @@
+# 3-2 Greetings
+
+friend_names = ["Albert Einstein", "Isac Newton", "Marie Curie", "Galileo Galilei"]
+
+message = "Hello " + friend_names[0] + "!"
+print(message)
+
+message = "Hello " + friend_names[1] + "!"
+print(message)
+
+message = "Hello " + friend_names[2] + "!"
+print(message)
+
+message = "Hello " + friend_names[3] + "!"
+print(message)

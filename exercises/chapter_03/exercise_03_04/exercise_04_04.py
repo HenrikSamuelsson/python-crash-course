2e72dcb52c23690c6f1b41cfff1948f18506293b
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2017-04-29 14:51:32 +0200

    Add solution to exercise 4.4.

---
+++ exercises/chapter_03/exercise_03_04/exercise_04_04.py
@@ -0,0 +1,15 @@
+# 3-4 Guest List
+
+guest_list = ["Albert Einstein", "Isac Newton", "Marie Curie", "Galileo Galilei"]
+
+message = "Hi " + guest_list[0] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[1] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[2] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[3] + " you are invited to dinner at 7 on saturday."
+print(message)

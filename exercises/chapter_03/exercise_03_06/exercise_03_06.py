877a7b7449a1d88c14633376a2dfaca8c619c26a
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2017-04-29 15:10:57 +0200

    Add solution to exercis 3.6.

---
+++ exercises/chapter_03/exercise_03_06/exercise_03_06.py
@@ -0,0 +1,60 @@
+# 3-6 Guest List
+
+guest_list = ["Albert Einstein", "Isac Newton", "Marie Curie", "Galileo Galilei"]
+
+message = "Hi " + guest_list[0] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[1] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[2] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[3] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+cancelation_message = guest_list[1] + " can not attend the dinner."
+print(cancelation_message)
+
+guest_list[1] = "Charles Darwin"
+
+message = "Hi " + guest_list[0] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[1] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[2] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[3] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "I have a bigger table now so three more people will be invited."
+print(message)
+
+guest_list.insert(0, "Stephen Hawking")
+guest_list.insert(2, "Louis Pasteur")
+guest_list.append("Nicolaus Copernicus")
+
+message = "Hi " + guest_list[0] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[1] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[2] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[3] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[4] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[5] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[6] + " you are invited to dinner at 7 on saturday."
+print(message)

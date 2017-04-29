94e6abcd9e03a28088b01ab18e3d09dfb3020fe0
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2017-04-29 14:57:38 +0200

    Add solution to exercise 3.5.

---
+++ exercises/chapter_03/exercise_03_05/exercise_03_05.py
@@ -0,0 +1,32 @@
+# 3-5 Guest List
+
+guest_list = ["Albert Einstein", "Isac Newton", "Marie Curie", "Galileo Galilei"]
+
+message = "Hi " + guest_list[0] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[1] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[2] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[3] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+cancelation_message = guest_list[1] + " can not attend the dinner."
+print(cancelation_message)
+
+guest_list[1] = "Charles Darwin"
+
+message = "Hi " + guest_list[0] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[1] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[2] + " you are invited to dinner at 7 on saturday."
+print(message)
+
+message = "Hi " + guest_list[3] + " you are invited to dinner at 7 on saturday."
+print(message)

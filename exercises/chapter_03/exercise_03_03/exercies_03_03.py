8d6ca433d33551cc1fe5c08edcf68ec65e5447b0
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2017-04-29 10:05:33 +0200

    Add solution to exercise 3.3.

---
+++ exercises/chapter_03/exercise_03_03/exercies_03_03.py
@@ -0,0 +1,6 @@
+# 3-3 Your Own List
+transportation = ["mountainbike", "teleportation", "Citroën DS3"]
+
+print("A " + transportation[0] + " is good when exercising in the woods.\n")
+print("The ultimate form of trarsportation must be " + transportation[1] + ".\n")
+print("Should I buy a " + transportation[2] + "?\n")

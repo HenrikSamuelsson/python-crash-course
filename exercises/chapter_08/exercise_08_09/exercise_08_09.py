9397e9da1c22ce7bb461586a929f70e48c39a9a1
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2017-06-11 22:20:05 +0200

    Add solution to exercise 8.9.

---
+++ exercises/chapter_08/exercise_08_09/exercise_08_09.py
@@ -0,0 +1,14 @@
+# 8-9. Magicians
+
+def show_magicians(names):
+	"""Print each magician in the list"""
+	for name in names:
+		print (name)
+
+magicians = [
+	"Harry Houdini",
+	"David Cooperfield",
+	"Criss Angel",
+	"David Blaine",
+]
+show_magicians(magicians)
